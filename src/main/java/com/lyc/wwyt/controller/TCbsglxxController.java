4f27ae0741970c5259682de02ce87e88d9b422d7
chenqi <chenqi92104@icloud.com>
2023-05-16 09:44:52 +0800

    ✨ 增加分页查询，给后面增加查询页面做准备

--- src/main/java/com/lyc/wwyt/controller/TCbsglxxController.java
+++ src/main/java/com/lyc/wwyt/controller/TCbsglxxController.java
@@ -1,14 +1,22 @@
 package com.lyc.wwyt.controller;
 
 import cn.allbs.idempotent.annotation.Idempotent;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.lyc.wwyt.config.log.annotation.SysLog;
 import com.lyc.wwyt.entity.TCbsglxxEntity;
 import com.lyc.wwyt.service.TCbsglxxService;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
+import org.springdoc.api.annotations.ParameterObject;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -55,7 +63,23 @@ public class TCbsglxxController {
     @Operation(description = "查询承包商管理信息表数据", summary = "查询承包商管理信息表数据", tags = {"查询所有数据"})
     @GetMapping
     @Idempotent(expireTime = 180, info = "3分钟内最多请求一次!")
-    public List<TCbsglxxEntity> updateById() {
+    public List<TCbsglxxEntity> selectAll() {
         return this.tCbsglxxService.list();
+    }
+
+    /**
+     * 分页查询当前账户下所有承包商信息
+     *
+     * @return List<TCbsglxxEntity> 分页当前账户下所有承包商信息
+     */
+    @Operation(description = "分页查询承包商管理信息表数据", summary = "分页查询承包商管理信息表数据", tags = {"分页查询所有数据"})
+    @GetMapping("page")
+    @Parameters({
+            @Parameter(description = "当前页", name = "current", in = ParameterIn.QUERY, required = true, schema = @Schema(implementation = Integer.class)),
+            @Parameter(description = "当前页条数", name = "size", in = ParameterIn.QUERY, required = true, schema = @Schema(implementation = Integer.class))
+    })
+    @Idempotent(expireTime = 180, info = "3分钟内最多请求一次!", key = "#page.current")
+    public IPage<TCbsglxxEntity> selectPage(@ParameterObject Page<TCbsglxxEntity> page, @ParameterObject TCbsglxxEntity tCbsglxxEntity) {
+        return this.tCbsglxxService.page(page, Wrappers.query(tCbsglxxEntity));
     }
 }
